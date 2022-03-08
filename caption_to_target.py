dc8705c42ae64a0ff0e712b8a9dbab5c2f6eda2f
madhavkr <madhavkr@umich.edu>
2022-03-08 14:42:49 -0500

    minor changes

--- caption_to_target.py
+++ caption_to_target.py
@@ -16,7 +16,7 @@ def extract_nouns(img_caps):
     img_caps = img_caps.apply(remove_stop)
 
     # extract nouns
-    is_noun = lambda pos: pos[:2] == 'NN'
+    is_noun = lambda pos: True if pos[0] == 'N' else False
     extract_nouns = np.vectorize(lambda x: ' '.join([word for (word, pos) 
                                                      in nltk.pos_tag(nltk.word_tokenize(x)) if is_noun(pos)]))
     return img_caps.apply(extract_nouns)
@@ -29,7 +29,7 @@ def max_word(wordlist):
         for j in range(n):
             wordFromList1 = wordnet.synsets(wordlist[i])
             wordFromList2 = wordnet.synsets(wordlist[j])
-            if wordFromList1 and wordFromList2: #Thanks to @alexis' note
+            if wordFromList1 and wordFromList2:
                 similarity[i, j] = wordFromList1[0].wup_similarity(wordFromList2[0])
 
     similarity = np.sum(similarity, axis=1)
@@ -38,12 +38,11 @@ def max_word(wordlist):
 
 # assuming we have a dataframe 'nouns' where rows are samples
 # and columns are the nouns in each caption for that sample
-def extract_targets(nouns, method):
+def extract_targets(nouns, method=2):
     # pick words that have highest similarity to every other target
     # if method is 1 extract 1 target per caption returns nxm
     #              2 extract 1 target per image returns nx1
     if method==1:
         return nouns.applymap(lambda x: max_word(re.split('\s+', x)))
-    if method==2:
+    else:
         return nouns.apply(lambda row: max_word(re.split('\s+', ' '.join(row.values.astype(str)))), axis=1)
-
